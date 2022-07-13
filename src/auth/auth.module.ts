006b35b17b6cf9abafd71cb650179735c27a64f9
Dunsin <85681801+Dunsin-cyber@users.noreply.github.com>
2022-07-13 13:06:37 +0100

    post and get request

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { jwtStrategy } from './strategy';
+
+@Module({
+  imports: [JwtModule.register({})],
+  controllers: [AuthController],
+  providers: [AuthService, jwtStrategy],
+})
+export class AuthModule {}
